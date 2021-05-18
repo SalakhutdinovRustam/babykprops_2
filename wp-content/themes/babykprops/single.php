7b689904bdff8959c5df2ef9a00a5eca0e96f659
Salakhutdinov Rustam <vasimovich@gmail.com>
2021-05-18 14:51:15 +0300

    UPDATED | Single news page

--- wp-content/themes/babykprops/single.php
+++ wp-content/themes/babykprops/single.php
@@ -5,6 +5,7 @@ $html_thumbnail = get_the_post_thumbnail();
 $date = get_the_date();
 $content = get_the_content();
 
+
 get_header(); 
 echo get_theme_page_title_block($title); 
 ?>
@@ -18,12 +19,11 @@ echo get_theme_page_title_block($title);
                     <div class="blog__details__content">
                         <div class="blog__details__item">
 												<?php echo $html_thumbnail; ?>
-                            <!-- <img src="<?php echo $temp_html_dir ?>img/blog/details/blog-details.jpg" alt=""> -->
                             <div class="blog__details__item__title">
                                 <span class="tip">Street style</span>
                                 <h4><?php echo $title ?></h4>
                                 <ul>
-                                    <li>by <span>Ema Timahe</span></li>
+                                    <li>by <span><?php the_author(); ?></span></li>
                                     <li><?php echo $date ?></li>
                                     <li>39 Comments</li>
                                 </ul>
@@ -53,6 +53,9 @@ echo get_theme_page_title_block($title);
                             </div>
                         </div>
                         <div class="blog__details__comment">
+												<?php 
+									comments_template();
+									?>
                             <h5>3 Comment</h5>
                             <a href="#" class="leave-btn">Leave a comment</a>
                             <div class="blog__comment__item">
@@ -104,64 +107,10 @@ echo get_theme_page_title_block($title);
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-4">
-                    <div class="blog__sidebar">
-                        <div class="blog__sidebar__item">
-                            <div class="section-title">
-                                <h4>Categories</h4>
-                            </div>
-                            <ul>
-                                <li><a href="#">All <span>(250)</span></a></li>
-                                <li><a href="#">Fashion week <span>(80)</span></a></li>
-                                <li><a href="#">Street style <span>(75)</span></a></li>
-                                <li><a href="#">Lifestyle <span>(35)</span></a></li>
-                                <li><a href="#">Beauty <span>(60)</span></a></li>
-                            </ul>
-                        </div>
-                        <div class="blog__sidebar__item">
-                            <div class="section-title">
-                                <h4>Feature posts</h4>
-                            </div>
-                            <a href="#" class="blog__feature__item">
-                                <div class="blog__feature__item__pic">
-                                    <img src="<?php echo $temp_html_dir ?>img/blog/sidebar/fp-1.jpg" alt="">
-                                </div>
-                                <div class="blog__feature__item__text">
-                                    <h6>Amf Cannes Red Carpet Celebrities Kend...</h6>
-                                    <span>Seb 17, 2019</span>
-                                </div>
-                            </a>
-                            <a href="#" class="blog__feature__item">
-                                <div class="blog__feature__item__pic">
-                                    <img src="<?php echo $temp_html_dir ?>img/blog/sidebar/fp-2.jpg" alt="">
-                                </div>
-                                <div class="blog__feature__item__text">
-                                    <h6>Amf Cannes Red Carpet Celebrities Kend...</h6>
-                                    <span>Seb 17, 2019</span>
-                                </div>
-                            </a>
-                            <a href="#" class="blog__feature__item">
-                                <div class="blog__feature__item__pic">
-                                    <img src="<?php echo $temp_html_dir ?>img/blog/sidebar/fp-3.jpg" alt="">
-                                </div>
-                                <div class="blog__feature__item__text">
-                                    <h6>Amf Cannes Red Carpet Celebrities Kend...</h6>
-                                    <span>Seb 17, 2019</span>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="blog__sidebar__item">
-                            <div class="section-title">
-                                <h4>Tags cloud</h4>
-                            </div>
-                            <div class="blog__sidebar__tags">
-                                <a href="#">Fashion</a>
-                                <a href="#">Street style</a>
-                                <a href="#">Diversity</a>
-                                <a href="#">Beauty</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+								<?php 
+									get_sidebar();
+									?>
+                 </div>
             </div>
         </div>
     </section>
